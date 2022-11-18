213a5afeb31e34347073a705abffbc7edb6343e6
sunkyu_park <noncefaln@gmail.com>
2022-11-18 20:51:34 +0900

    tailwindCss로 ToDo 앱 스타일링 해주기

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -19,15 +19,15 @@ export default function List({ todoData, setTodoData }) {
       {todoData.map((data) => (
         <div key={data.id}>
           <div className='flex items-center justify-between w-full px-4 py-1 my-2 text-gray-500 bg-gray-100 border rounded'>
-            <div>
+            <div className='items-center'>
               <input
                 type='checkbox'
                 onChange={() => handleCompleteChange(data.id)}
               />
-              <span>{data.title}</span>
+              <span className={data.completed ? "line-through" : undefined}>{data.title}</span>
             </div>
             <div>
-              <button onClick={() => handleClick(data.id)}>X</button>
+              <button className='px-4 py-2 float-right' onClick={() => handleClick(data.id)}>X</button>
             </div>
           </div>
         </div>
