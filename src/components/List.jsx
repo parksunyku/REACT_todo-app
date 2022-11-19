c42b04e750927e32a63c569d12a8e3a89724d6b4
sunkyu_park <noncefaln@gmail.com>
2022-11-19 10:07:25 +0900

    컴포넌트 내 분리, List 컴포넌트 생성

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,12 @@
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-
-export default function List({ todoData, setTodoData }) {
+const List = ({
+  id,
+  title,
+  completed,
+  todoData,
+  setTodoData,
+  provided,
+  snapshot,
+}) => {
   const handleCompleteChange = (id) => {
     let newTodoData = todoData.map((data) => {
       if (data.id === id) {
@@ -16,74 +22,30 @@ export default function List({ todoData, setTodoData }) {
     setTodoData(newTodoData);
   };
 
-  const handleEnd = (result) => {
-    console.log(result);
-
-    if (!result.destination) return;
-
-    const newTodoData = [...todoData];
-
-    // 1. 변경시키는 아이템을 배열에서 지워줍니다.
-    // 2. return 값으로 지워진 아이템을 잡아줍니다.
-    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
-
-    // 원하는 자리에 reorderedItem을 insert 해줍니다.
-    newTodoData.splice(result.destination.index, 0, reorderedItem);
-    setTodoData(newTodoData);
-  };
-  //
   return (
-    <div>
-      <DragDropContext onDragEnd={handleEnd}>
-        <Droppable droppableId='todo'>
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {todoData.map((data, index) => (
-                <Draggable
-                  key={data.id}
-                  draggableId={data.id.toString()}
-                  index={index}
-                >
-                  {(provided, snapshot) => (
-                    <div
-                      key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      className={`${
-                        snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'
-                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-500 border rounded`}
-                    >
-                      <div className='items-center'>
-                        <input
-                          type='checkbox'
-                          onChange={() => handleCompleteChange(data.id)}
-                        />
-                        <span
-                          className={
-                            data.completed ? 'line-through' : undefined
-                          }
-                        >
-                          {data.title}
-                        </span>
-                      </div>
-                      <div>
-                        <button
-                          className='px-4 py-2 float-right'
-                          onClick={() => handleClick(data.id)}
-                        >
-                          X
-                        </button>
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <div
+      key={id}
+      {...provided.draggableProps}
+      ref={provided.innerRef}
+      {...provided.dragHandleProps}
+      className={`${
+        snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'
+      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-500 border rounded`}
+    >
+      <div className='items-center'>
+        <input type='checkbox' onChange={() => handleCompleteChange(id)} />
+        <span className={completed ? 'line-through' : undefined}>{title}</span>
+      </div>
+      <div>
+        <button
+          className='px-4 py-2 float-right'
+          onClick={() => handleClick(id)}
+        >
+          X
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+export default List;
---
+++ src/components/Lists.jsx
@@ -0,0 +1,53 @@
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import List from './List';
+export default function Lists({ todoData, setTodoData }) {
+  const handleEnd = (result) => {
+    console.log(result);
+
+    if (!result.destination) return;
+
+    const newTodoData = [...todoData];
+
+    // 1. 변경시키는 아이템을 배열에서 지워줍니다.
+    // 2. return 값으로 지워진 아이템을 잡아줍니다.
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+    // 원하는 자리에 reorderedItem을 insert 해줍니다.
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
+    setTodoData(newTodoData);
+  };
+  //
+  return (
+    <div>
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId='todo'>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <List
+                      key={data.id}
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
